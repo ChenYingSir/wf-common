52234d978699b1888617272f00cd272225735852
chenying <18855070840@163.com>
2019-03-12 22:24:42 +0800

    update OrderProejct

--- wf-common-dao/src/main/java/com/zb/mapper/WfOrderItemMapper.java
+++ wf-common-dao/src/main/java/com/zb/mapper/WfOrderItemMapper.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface WfOrderItemMapper {
 	//陈营
-	//test
+    //test
 	public WfOrderItem getWfOrderItemById(@Param(value = "id") Long id)throws Exception;
 
 	public List<WfOrderItem> getWfOrderItemListByMap(Map<String,Object> param)throws Exception;
